d56cf1d60d9ce4e94e39e9f0e83b55123a123355
kawtar <kaw200tar@gmail.com>
2024-12-19 00:54:02 +0100

    refoctor cycle4

--- ArabicRomanNumerals.java
+++ ArabicRomanNumerals.java
@@ -2,6 +2,15 @@ package com.pantxi.romain;
 
 public class ArabicRomanNumerals {
     public static String convert(int n) {
-        if (n == 4) return "IV";
-        return "I";
+        StringBuilder roman = new StringBuilder();
+        int[] values = {4, 1};
+        String[] symbols = {"IV", "I"};
+
+        for (int i = 0; i < values.length; i++) {
+            while (n >= values[i]) {
+                roman.append(symbols[i]);
+                n -= values[i];
+            }
+        }
+        return roman.toString();
     }}
