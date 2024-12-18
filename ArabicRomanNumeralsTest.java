414f6fde89121873c0e02cee07f805fe3d2768aa
kawtar <kaw200tar@gmail.com>
2024-12-18 22:39:23 +0100

    cycle3

--- ArabicRomanNumeralsTest.java
+++ ArabicRomanNumeralsTest.java
@@ -7,8 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ArabicRomanNumeralsTest {
 
     @Test
-    void testConvert1() {
-        assertEquals("I", ArabicRomanNumerals.convert(1));
+    void testConvert4() {
+        assertEquals("IV", ArabicRomanNumerals.convert(4));
     }
-
 }
