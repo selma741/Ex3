7057d694b249d9bf801c10f6e7db99451840f1c3
kawtar <kaw200tar@gmail.com>
2024-12-19 00:38:54 +0100

    cycle1

---
+++ arabicromTest.java
@@ -0,0 +1,14 @@
+
+    package com.pantxi.romain;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+    public class arabicromTest {
+
+        @Test
+        void testConvert1() {
+            assertEquals("I", ArabicRomanNumerals.convert(1));
+        }}
+
