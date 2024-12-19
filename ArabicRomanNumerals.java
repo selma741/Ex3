7393a5a82b1df6b866ffe47a72245e45c8877f44
kawtar <kaw200tar@gmail.com>
2024-12-19 01:23:31 +0100

    Refactor cycle 12

--- ArabicRomanNumerals.java
+++ ArabicRomanNumerals.java
@@ -3,6 +3,6 @@ package com.pantxi.romain;
 public class ArabicRomanNumerals {
         public static String convert(int n) {
             int[] values = {10, 9, 5, 4, 1};
-            String[] symbols = {"X", "IX", "V", "IV", "I"};
+            String[] symbols = {"X", "IX", "V", "IV", "I"};;
             return "n";
         }}
