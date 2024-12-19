d5765e95983f89cd75459bce2a7f20fa9c5257b5
kawtar <kaw200tar@gmail.com>
2024-12-19 01:05:43 +0100

    cycle 6

--- ArabicRomanNumerals.java
+++ ArabicRomanNumerals.java
@@ -3,14 +3,7 @@ package com.pantxi.romain;
 public class ArabicRomanNumerals {
     public static String convert(int n) {
         StringBuilder roman = new StringBuilder();
-        int[] values = {4, 1};
-        String[] symbols = {"IV", "I"};
-
-        for (int i = 0; i < values.length; i++) {
-            while (n >= values[i]) {
-                roman.append(symbols[i]);
-                n -= values[i];
-            }
-        }
-        return roman.toString();
+        int[] values = {5, 4, 1};
+        String[] symbols = {"V", "IV", "I"};
+        return "";
     }}
